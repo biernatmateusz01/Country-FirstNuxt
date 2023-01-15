49deac3aba51c13bf266c9e8b02e372e7ff33184
biernatmateusz01 <biernat521@gmai.com>
2023-01-15 19:25:13 +0100

    update country site view

--- pages/country/[id]-[link-rewrite].vue
+++ pages/country/[id]-[link-rewrite].vue
@@ -1,6 +1,52 @@
 <template>
-  <div class="container block m-auto p-4 xl:p-2 mb-20">
-    <span>{{ countryInfs }}</span>
+  <div class="container block m-auto p-4 mb-20 py-20 text-base sm:text-lg xl:text-xl">
+    <div class="flex flex-col gap-2">
+      <div class="w-full flex gap-4 justify-between border-b border-emerald-500 p-2">
+        <span>Name:</span>
+        <span class="truncate">{{ countryInfs[0].name.official }}</span>
+      </div>
+      <div class="w-full flex justify-between border-b border-emerald-500 p-2">
+        <span>Capital:</span>
+
+        <div class="flex flex-col gap-1">
+          <span v-for="capital in countryInfs[0].capital" :key="capital">{{
+            capital
+          }}</span>
+        </div>
+      </div>
+      <div class="w-full flex justify-between border-b border-emerald-500 p-2">
+        <span>Region:</span>
+        <span>{{ countryInfs[0].region }}</span>
+      </div>
+
+      <div class="w-full flex justify-between border-b border-emerald-500 p-2">
+        <span>Subregion:</span>
+        <span>{{ countryInfs[0].subregion }}</span>
+      </div>
+
+      <div class="w-full flex justify-between border-b border-emerald-500 p-2">
+        <span>Flag:</span>
+        <span>{{ countryInfs[0].flag }}</span>
+      </div>
+      <div class="w-full flex justify-between border-b border-emerald-500 p-2">
+        <span>Population:</span>
+        <span>{{ countryInfs[0].population }}</span>
+      </div>
+      <div class="w-full flex justify-between border-b border-emerald-500 p-2">
+        <span>TimeZones:</span>
+        <div class="flex flex-col gap-1">
+          <span v-for="zones in countryInfs[0].timezones" :key="zones">{{
+            zones
+          }}</span>
+        </div>
+      </div>
+
+      <img
+        :src="countryInfs[0].flags.png"
+        alt="flag"
+        class="block m-auto w-1/5 mt-4"
+      />
+    </div>
   </div>
 </template>
 
